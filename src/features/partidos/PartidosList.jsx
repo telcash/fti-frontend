4abaf56c3645e705e27b79f4b4471b036a86ae76
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2024-07-15 12:45:22 +0200

    Corregido resultado no sale en algunos partidos

--- src/features/partidos/PartidosList.jsx
+++ src/features/partidos/PartidosList.jsx
@@ -71,7 +71,7 @@ const PartidosList = () => {
                                 <TableCell align="center">{dayjs(partido.fecha).format('DD/MM/YYYY')}</TableCell>
                                 <TableCell align="center">{(partido.equipoLocal && partido.equipoLocal.nombre) ?? ''}</TableCell>
                                 <TableCell align="center">{(partido.equipoVisitante && partido.equipoVisitante.nombre) ?? ''}</TableCell>
-                                <TableCell align="center">{(partido.golesLocal && partido.golesVisitante) ? `${partido.golesLocal} - ${partido.golesVisitante}` : ''}</TableCell>
+                                <TableCell align="center">{`${partido.golesLocal} - ${partido.golesVisitante}`}</TableCell>
                                 <TableCell align="center">{partido.id}</TableCell>
                                 <TableCell align="center">
                                     <div className="action-buttons">
